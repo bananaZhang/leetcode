01bef4cf2efcd15d0ae435477282aead71955408
zhangjunyang <95231885@qq.com>
2019-01-24 20:08:49 +0800

    注释

--- src/Leetcode007.java
+++ src/Leetcode007.java
@@ -7,15 +7,18 @@
 public class Leetcode007 {
     public int reverse(int x) {
         int result = 0;
-        int count = 0;
+        int count = 0;// 统计入参x的位数
         while (x != 0) {
             result = result * 10 + x % 10;
             x /= 10;
             count ++;
+            // 一旦到9位时需要判断溢出问题
             if (count == 9 && x != 0) {
+                // 正数未溢出的情况
                 if (x > 0 && result <= Integer.MAX_VALUE / 10 && x <= Integer.MAX_VALUE % 10) {
                     continue;
                 }
+                // 负数未溢出的情况
                 if (x < 0 && result >= Integer.MIN_VALUE / 10 && x >= Integer.MIN_VALUE % 10) {
                     continue;
                 }
