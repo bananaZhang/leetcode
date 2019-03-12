588a1275d44b38ac4b062825e080369fd5385d23
zhangjunyang <95231885@qq.com>
2019-03-12 19:50:35 +0800

    047

--- src/Leetcode047.java
+++ src/Leetcode047.java
@@ -12,12 +12,11 @@ public class Leetcode047 {
 
     public static void main(String[] args) {
         Leetcode047 test = new Leetcode047();
-        System.out.println(test.permuteUnique(new int[]{1, 1, 2}));
+        System.out.println(test.permuteUnique(new int[]{1,1,2,2}));
     }
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
 
         permuteNums(nums, 0, nums.length - 1, result);
         return result;
@@ -28,13 +27,11 @@ public class Leetcode047 {
             result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
             return;
         }
-        for (int i = start + 1; i <= end; i++) {
-            if (nums[start] == nums[i]) {
+        for (int i = start; i <= end; i++) {
+            if (!isSwap(nums, start, i)) {
                 continue;
             }
             swap(nums, start, i);
-            result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-
             permuteNums(nums, start + 1, end, result);
             swap(nums, start, i);
         }
@@ -44,5 +41,14 @@ public class Leetcode047 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public boolean isSwap(int[] nums, int begin, int end) {
+        for (int i = begin; i < end; i++) {
+            if (nums[i] == nums[end]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
