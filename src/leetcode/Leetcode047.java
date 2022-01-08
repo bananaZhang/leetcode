d02958ba7e3272179c0554c6fc064ff79632fbaf
bananaZhang <jyzhang1994@163.com>
2022-01-08 22:15:42 +0800

    回溯法全排列2，有重复数字的情况

--- src/leetcode/Leetcode047.java
+++ src/leetcode/Leetcode047.java
@@ -1,56 +1,56 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 /**
  * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * 回溯法
  * @author ZJY
  * @date 2019/3/11 19:04
  */
 public class Leetcode047 {
 
-    public static void main(String[] args) {
-        Leetcode047 test = new Leetcode047();
-        System.out.println(test.permuteUnique(new int[]{1,1,2,2}));
-    }
+    private List<List<Integer>> resultList = new ArrayList<>();
 
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        permuteNums(nums, 0, nums.length - 1, result);
-        return result;
+        backtrack(nums, 0, new ArrayList<>());
+        return resultList;
     }
 
-    public void permuteNums(int[] nums, int start, int end, List<List<Integer>> result) {
-        if (start == end) {
-            result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+    /**
+     * 其实这个全排列算法就是固定一个数的位置(index)，然后从下一位数再开始全排列(递归过程)...直到最后一位数，
+     * 模拟手动全排列的过程。所以如果要去重的话，只要控制每次排列时，固定的那个数是不一样的就行了。
+     * 因为固定的数不一样，那从这个数开始产生的全排列就不一样。所以只要让每次的index位置的数不一样就行
+     */
+    private void backtrack(int[] nums, int index, List<Integer> result) {
+        if (index == nums.length) {
+            resultList.add(new ArrayList<>(result));
             return;
         }
-        for (int i = start; i <= end; i++) {
-            if (!isSwap(nums, start, i)) {
+        // 存储位于过index位置的数
+        Set<Integer> set = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            // index位置没有存在过该数才走下面的递归流程
+            if (i != index && set.contains(nums[i])) {
                 continue;
             }
-            swap(nums, start, i);
-            permuteNums(nums, start + 1, end, result);
-            swap(nums, start, i);
+            result.add(nums[i]);
+            set.add(nums[i]);
+            swap(nums, index, i);
+            backtrack(nums, index + 1, result);
+            swap(nums, index, i);
+            result.remove(result.size() - 1);
         }
     }
 
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] nums, int p, int q) {
+        int temp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = temp;
     }
 
-    public boolean isSwap(int[] nums, int begin, int end) {
-        for (int i = begin; i < end; i++) {
-            if (nums[i] == nums[end]) {
-                return false;
-            }
-        }
-        return true;
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 2, 1, 1};
+        System.out.println(new Leetcode047().permuteUnique(nums));
     }
 }
