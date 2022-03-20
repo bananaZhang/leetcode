add8b9a079c66b1b2de9dfa9ca2a3fa1eaf49b90
bananaZhang <jyzhang1994@163.com>
2022-03-20 22:49:25 +0800

    递归法反转链表

--- src/leetcode/Leetcode206.java
+++ src/leetcode/Leetcode206.java
@@ -21,6 +21,19 @@ public class Leetcode206 {
         return prev;
     }
 
+    public ListNode reverseListRecursion(ListNode head) {
+        return reverse(null, head);
+    }
+
+    private ListNode reverse(ListNode pre, ListNode cur) {
+        if (cur == null) {
+            return pre;
+        }
+        ListNode next = cur.next;
+        cur.next = pre;
+        return reverse(cur, next);
+    }
+
     public static void main(String[] args) {
         ListNode head = LianbiaoUtil.buildLiaobian();
         LianbiaoUtil.soutLianbiao(head);
