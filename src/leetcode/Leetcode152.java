20b89b9f2e1ed1653219ec2bf37ebaaf8af537ef
bananaZhang <jyzhang1994@163.com>
2022-05-02 22:23:56 +0800

    动态规划-最大乘积子数组

---
+++ src/leetcode/Leetcode152.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 乘积最大子数组
+ * 动态规划（两个dp数组）
+ * @author: ZJY
+ * @date: 2022/5/2 10:22 下午
+ */
+public class Leetcode152 {
+    public int maxProduct(int[] nums) {
+        int[] minDp = Arrays.copyOf(nums, nums.length);
+        int[] maxDp = Arrays.copyOf(nums, nums.length);
+        for (int i = 1; i < nums.length; i++) {
+            maxDp[i] = Math.max(maxDp[i - 1] * nums[i], Math.max(nums[i], minDp[i - 1] * nums[i]));
+            minDp[i] = Math.min(minDp[i - 1] * nums[i], Math.min(nums[i], maxDp[i - 1] * nums[i]));
+        }
+        return Arrays.stream(maxDp).max().getAsInt();
+    }
+}
