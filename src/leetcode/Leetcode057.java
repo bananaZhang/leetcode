4620e638daa4164cfbdaf9eb9ea2065ffa970aff
bananaZhang <jyzhang1994@163.com>
2022-05-11 22:36:17 +0800

    插入区间

---
+++ src/leetcode/Leetcode057.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 插入区间
+ * @author: ZJY
+ * @date: 2022/5/11 10:34 下午
+ */
+public class Leetcode057 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int left = newInterval[0];
+        int right = newInterval[1];
+        List<int[]> resList = new ArrayList<>();
+        boolean isInsert = false;
+        for (int[] interval : intervals) {
+            // 区间左边界大于right
+            if (interval[0] > right) {
+                // 这里只能插一次，否则数据重复
+                if (!isInsert) {
+                    resList.add(new int[]{left, right});
+                    isInsert = true;
+                }
+                resList.add(interval);
+            } else if (interval[1] < left) {
+                resList.add(interval);
+            } else {
+                left = Math.min(interval[0], left);
+                right = Math.max(interval[1], right);
+            }
+        }
+        // 没有插入过left right则插入
+        if (!isInsert) {
+            resList.add(new int[]{left, right});
+        }
+        int[][] resultArr = new int[resList.size()][2];
+        for (int i = 0; i < resList.size(); i++) {
+            resultArr[i][0] = resList.get(i)[0];
+            resultArr[i][1] = resList.get(i)[1];
+        }
+        return resultArr;
+    }
+}
