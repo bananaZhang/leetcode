8b18e470599808f6ddcf62358a63bc5d012a0702
bananaZhang <jyzhang1994@163.com>
2022-05-22 17:06:04 +0800

    二叉树最大深度

---
+++ src/leetcode/Leetcode102.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+import java.util.*;
+
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * @author: ZJY
+ * @date: 2022/2/19 7:51 下午
+ */
+public class Leetcode102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        if (root == null) {
+            return resultList;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (count > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                count --;
+            }
+            resultList.add(list);
+        }
+        return resultList;
+    }
+}
--- src/leetcode/Leetcode104.java
+++ src/leetcode/Leetcode104.java
@@ -2,37 +2,35 @@ package leetcode;
 
 import leetcode.common.TreeNode;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
- * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * 二叉树的最大深度
  * @author: ZJY
- * @date: 2022/2/19 7:51 下午
+ * @date: 2022/5/22 4:59 下午
  */
 public class Leetcode104 {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        if (root == null) {
-            return resultList;
-        }
+    public int maxDepth(TreeNode root) {
         Deque<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return 0;
+        }
         queue.offer(root);
+        int level = 0;
         while (!queue.isEmpty()) {
+            level++;
             int count = queue.size();
-            List<Integer> list = new ArrayList<>();
-            while (count > 0) {
+            while (count-- > 0) {
                 TreeNode node = queue.poll();
-                list.add(node.val);
                 if (node.left != null) {
                     queue.offer(node.left);
                 }
                 if (node.right != null) {
                     queue.offer(node.right);
                 }
-                count --;
             }
-            resultList.add(list);
         }
-        return resultList;
+        return level;
     }
 }
