8c2e2843bc0664d0959fbbdfa3ef42b480073ba5
bananaZhang <jyzhang1994@163.com>
2022-05-26 22:37:23 +0800

    最小编辑距离

---
+++ src/leetcode/Leetcode072.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * 最小编辑距离
+ * 动态规划
+ * @author: ZJY
+ * @date: 2022/5/26 10:31 下午
+ */
+public class Leetcode072 {
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1+1][len2+1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= len2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                // 要从两个字符串的第一个字符开始遍历
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    int min = Math.min(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.min(min, dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
