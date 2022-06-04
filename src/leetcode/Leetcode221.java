ef4573c53049de9a696d224d214ddbb0d9ade1b5
bananaZhang <jyzhang1994@163.com>
2022-06-04 17:36:07 +0800

    最大正方形

---
+++ src/leetcode/Leetcode221.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+/**
+ * 最大正方形
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ * 动态规划
+ * @author: ZJY
+ * @date: 2022/6/4 5:35 下午
+ */
+public class Leetcode221 {
+    public int maximalSquare(char[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == '1') {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        return max * max;
+    }
+}
