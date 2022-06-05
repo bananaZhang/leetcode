abc0e49bd8032c1a0f39e1cd9d4bb3398ba2066d
bananaZhang <jyzhang1994@163.com>
2022-06-05 12:50:22 +0800

    最大矩形

---
+++ src/leetcode/Leetcode085.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+
+import java.util.Stack;
+
+/**
+ * 最大矩形
+ * 这一题可以抽象成84题
+ * @author: ZJY
+ * @date: 2022/6/4 10:40 下午
+ */
+public class Leetcode085 {
+    public int maximalRectangle(char[][] matrix) {
+        int max = 0;
+        int[] heights = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '0') {
+                    heights[j] = 0;
+                } else {
+                    heights[j]++;
+                }
+            }
+            max = Math.max(max, largestRectangleArea(heights));
+        }
+        return max;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        int[] newHeights = new int[heights.length + 2];
+        // 首尾各添加一个0便于处理
+        for (int i = 0; i < heights.length; i++) {
+            newHeights[i+1] = heights[i];
+        }
+        for (int i = 0; i < newHeights.length; i++) {
+            while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
+                int top = stack.pop();
+                int width = i - stack.peek() - 1;
+                max = Math.max(max, newHeights[top] * width);
+            }
+            stack.add(i);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println(new Leetcode085().maximalRectangle(matrix));
+    }
+}
--- src/leetcode/Leetcode221.java
+++ src/leetcode/Leetcode221.java
@@ -17,11 +17,6 @@ public class Leetcode221 {
                 }
             }
         }
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i] == '1') {
-                dp[0][i] = 1;
-            }
-        }
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
