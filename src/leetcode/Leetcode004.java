de227b385777d90d5104e34009969ca0c14f005f
zhangjy <95231885@qq.com>
2024-09-10 20:49:36 +0800

    Leetcode004

--- src/leetcode/Leetcode004.java
+++ src/leetcode/Leetcode004.java
@@ -2,44 +2,43 @@ package leetcode;
 
 /**
  * 寻找两个正序数组的中位数
- * 字节题
+ * 字节题 https://www.acwing.com/solution/content/14344/
  * @author: ZJY
  * @date: 2022/4/24 10:32 下午
  */
 public class Leetcode004 {
-    /**
-     * hard题，有个比较牛b的方法使用二分法，但我不会写
-     */
 
+    // 分别找第 (m+n+1)/2 个，和 (m+n+2)/2 个，然后求其平均值即可，这对奇偶数均适用。
+    // 若 m+n 为奇数的话，那么其实 (m+n+1)/2 和 (m+n+2)/2 的值相等，相当于两个相同的数字相加再除以2，还是其本身。
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len = nums1.length + nums2.length;
-        int[] nums = new int[len / 2 + 1];
-        int i = 0, j = 0;
-        while (i < nums1.length && j < nums2.length && i+j < nums.length) {
-            if (nums1[i] > nums2[j]) {
-                nums[i+j] = nums2[j];
-                j++;
-            } else {
-                nums[i+j] = nums1[i];
-                i++;
-            }
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int left = (len1 + len2 + 1) / 2;
+        int right = (len1 + len2 + 2) / 2;
+        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0d;
+    }
+
+    private int findKth(int[] nums1, int i, int[] nums2, int j, int k) {
+        // 起始位置大于数组长度时，只需要从另一个数组找就行了
+        if (i >= nums1.length) {
+            return nums2[j + k - 1];
         }
-        // 没有填充满nums数组的情况
-        if (nums[nums.length - 1] == 0) {
-            if (i == nums1.length) {
-                while (i+j < nums.length) {
-                    nums[i+j] = nums2[j++];
-                }
-            } else {
-                while (i+j < nums.length) {
-                    nums[i+j] = nums1[i++];
-                }
-            }
+        if (j >= nums2.length) {
+            return nums1[i + k - 1];
         }
-        if (len % 2 > 0) {
-            return nums[nums.length - 1];
+        // k=1时直接拿第一个元素
+        if (k == 1) {
+            return Math.min(nums1[i], nums2[j]);
         }
-        return ((double)nums[nums.length - 1] + (double)nums[nums.length - 2]) / 2;
+        // 分别在nums1和nums2中找第 k/2 个元素，如果某个数组的个数都不到 k/2 个，自然无法淘汰，所以将其对应的 midVal 值设为整型
+        // 最大值（目的是要在 nums1 或者 nums2 中先淘汰 K/2 个较小的数字，判断的依据就是看 midVal1 和 midVal2 谁更小）
+        int midVal1 = (i + k/2 - 1 < nums1.length) ? nums1[i + k/2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = (j + k/2 - 1 < nums2.length) ? nums2[j + k/2 - 1] : Integer.MAX_VALUE;
+        if (midVal1 < midVal2) {
+            return findKth(nums1, i + k/2, nums2, j, k - k/2);
+        } else {
+            return findKth(nums1, i, nums2, j + k/2, k - k/2);
+        }
     }
 
     public static void main(String[] args) {
