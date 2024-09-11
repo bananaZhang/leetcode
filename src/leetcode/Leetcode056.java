ee8ce101ede073616a281f67da3bf1931d165bda
zhangjy <95231885@qq.com>
2024-09-11 21:16:56 +0800

    更新Leetcode056

--- src/leetcode/Leetcode056.java
+++ src/leetcode/Leetcode056.java
@@ -1,7 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * 合并区间
@@ -12,26 +14,20 @@ import java.util.Comparator;
  */
 public class Leetcode056 {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-        int[][] res = new int[intervals.length][2];
-        int j = 0;
-        res[j][0] = intervals[0][0];
-        res[j][1] = intervals[0][1];
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        List<int[]> result = new ArrayList<>();
+        int left = intervals[0][0];
+        int right = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            // 区间start小于上一区间的end
-            if (intervals[i][0] <= res[j][1]) {
-                res[j][1] = Math.max(res[j][1], intervals[i][1]);
+            if (intervals[i][0] > right) {
+                result.add(new int[]{left, right});
+                left = intervals[i][0];
+                right = intervals[i][1];
             } else {
-                j++;
-                res[j][0] = intervals[i][0];
-                res[j][1] = intervals[i][1];
+                right = Math.max(right, intervals[i][1]);
             }
         }
-        return Arrays.copyOf(res, j + 1);
+        result.add(new int[]{left, right});
+        return result.toArray(int[][]::new);
     }
 }
