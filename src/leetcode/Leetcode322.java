3b912716bfa743fb1003bf0ed69bd82a2a161ace
zhangjy <95231885@qq.com>
2024-09-12 22:18:32 +0800

    更新Leetcode322

--- src/leetcode/Leetcode322.java
+++ src/leetcode/Leetcode322.java
@@ -13,6 +13,27 @@ import java.util.Arrays;
 public class Leetcode322 {
     private int[] minStep;
 
+    public int coinChange2(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                dp[coins[i]] = 1;
+            }
+        }
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) {
             return 0;
