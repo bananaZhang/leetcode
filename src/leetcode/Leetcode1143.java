408490ab491ee27ee9a93e5fe56708d7863b7c50
zhangjy <95231885@qq.com>
2024-09-27 22:28:08 +0800

    update 1143

--- src/leetcode/Leetcode1143.java
+++ src/leetcode/Leetcode1143.java
@@ -12,12 +12,14 @@ public class Leetcode1143 {
         int len1 = text1.length();
         int len2 = text2.length();
         int[][] dp = new int[len1+1][len2+1];
-        for (int i = 0; i < len1; i++) {
-            for (int j = 0; j < len2; j++) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i+1][j+1] = dp[i][j] + 1;
+        for (int i = 1; i <= len1; i++) {
+            char c1 = text1.charAt(i-1);
+            for (int j = 1; j <= len2; j++) {
+                char c2 = text2.charAt(j-1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[i+1][j+1] = Math.max(dp[i][j], Math.max(dp[i+1][j], dp[i][j+1]));
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
