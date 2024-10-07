99a6498d969ab252bfd8220b2953796c768f18f3
zhangjy <95231885@qq.com>
2024-10-07 16:13:07 +0800

    update 160

--- src/leetcode/Leetcode160.java
+++ src/leetcode/Leetcode160.java
@@ -9,28 +9,12 @@ import leetcode.common.ListNode;
  */
 public class Leetcode160 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) {
-            return null;
-        }
         ListNode curA = headA;
         ListNode curB = headB;
-        int count = 2;
-        // 下面可以优化，没有相交的话，最后也是 curA == curB == null，相交的话，返回的 curA 或者 curB 就是相交点
-        while (count >= 0) {
-            if (curA == curB) {
-                return curA;
-            }
-            curA = curA.next;
-            curB = curB.next;
-            if (curA == null) {
-                curA = headB;
-                count--;
-            }
-            if (curB == null) {
-                curB = headA;
-                count--;
-            }
+        while (curA != curB) {
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
         }
-        return null;
+        return curA;
     }
 }
